71a4a8102a9421215655f38208d0a706b838e9da
Zoiye van Leeuwen <zoiyevanleeuwen@icloud.com>
2025-12-10 10:55:26 +0100

    fix: change margins to better fit mobile devices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@
     <SolarSystemBackground />
 
     <!-- Main Content -->
-    <div class="relative z-10 py-12 px-4">
-      <div class="container mx-auto px-4 py-8">
+    <div class="relative z-10 py-6 sm:py-12 px-4 sm:px-6 lg:px-8">
+      <div class="container mx-auto px-2 sm:px-4 py-4 sm:py-8">
         <AboutSection />
         <ProjectsSection :projects="projects" />
       </div>
